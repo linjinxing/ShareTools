d214479d31a87546ea0d65a7d40dfb172815639d
ls <lsw@lswen.com>
2014-03-24 22:46:32 +0800

    first commit

---
+++ ShareTools/ShareTools/ShareTools.h
@@ -0,0 +1,13 @@
+//
+//  ShareTools.h
+//  ShareTools
+//
+//  Created by linjinxing on 14-3-24.
+//  Copyright (c) 2014年 ST. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShareTools : NSObject
+
+@end
---
+++ ShareTools/ShareTools/ShareTools.m
@@ -0,0 +1,13 @@
+//
+//  ShareTools.m
+//  ShareTools
+//
+//  Created by linjinxing on 14-3-24.
+//  Copyright (c) 2014年 ST. All rights reserved.
+//
+
+#import "ShareTools.h"
+
+@implementation ShareTools
+
+@end

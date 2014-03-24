5eb1bb1b2163b93ff89749fee001ea39d1095f18
ls <lsw@lswen.com>
2014-03-24 22:42:53 +0800

    Initial Commit

---
+++ ShareToolsDemo/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  ShareToolsDemo
+//
+//  Created by linjinxing on 14-3-24.
+//  Copyright (c) 2014年 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShareToolsDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShareToolsDemo
+//
+//  Created by linjinxing on 14-3-24.
+//  Copyright (c) 2014年 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
